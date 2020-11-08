71ba26bc56219805480f1a0b291eee68466a63c6
emile kamana <emileizere@gmail.com>
2020-11-09 01:07:49 +0200

    edited readme

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   showDetails: boolean;
-  constructor(public name: string, public content: string, public author: string, public vote: number, public time: Date){
+  constructor(public name: string, public content: string, public author: string, public vote: number, public timePost: Date){
     this.showDetails = false;
   }
 }
--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -6,11 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimeCountPipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
-    let today : Date = new Date();
-    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-    var dateDifference = Math.abs(value - todayWithNoTime);
-    var dateDifferenceSeconds = dateDifference * 0.001;
-    var dateCounter = dateDifferenceSeconds / 86400;
-    return dateCounter;
+    var now = new Date();
+    var diff = Math.abs(now.getTime() - value.getTime());
+    var diffDays = Math.ceil(​diff / (1000 * 3600 * 24));
+    return diffDays;
   }
 }
