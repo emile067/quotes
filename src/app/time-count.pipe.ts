816e11549514e1035669f7fa013ec91b717d996d
emile kamana <emileizere@gmail.com>
2020-11-09 12:57:04 +0200

    changes time update to minutes ago

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -6,9 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimeCountPipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
-    var now = new Date();
-    var diff = Math.abs(now.getTime() - value.getTime());
-    var diffDays = Math.ceil(​diff / (1000 * 3600 * 24));
-    return diffDays;
+    var dateToday: any = new Date();
+    setInterval(() => {
+      dateToday = new Date();
+    }, 100);
+    var diffMs = (dateToday - value); // milliseconds between now & Christmas
+    var diffDays = Math.floor(diffMs / 86400000); // days
+    var diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
+    var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
+    // alert(diffDays + " days, " + diffHrs + " hours, " + diffMins + " minutes until Christmas 2009 =)");
+    return diffMins;
   }
 }
